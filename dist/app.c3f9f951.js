373d4d8f4fd641cd917ba8319ee1cf11597e31ef
amapic <amaury.pichat@gmail.com>
2023-03-27 16:45:47 +0000

    update img

--- dist/app.c3f9f951.js
+++ dist/app.c3f9f951.js
@@ -36543,7 +36543,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43447" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41533" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
