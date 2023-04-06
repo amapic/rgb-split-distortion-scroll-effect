abcdb205b98d5e2c8e032438946ff62f0599d1c8
amapic <amaury.pichat@gmail.com>
2023-04-06 09:17:20 +0000

    on essaye de gerer les couleurs

--- dist/style.e308ff8e.js
+++ dist/style.e308ff8e.js
@@ -172,7 +172,7 @@ module.exports = reloadCSS;
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"/project/rgb-split-distortion-scroll-effect/Harmond-ExtraBoldExpanded.otf":[["Harmond-ExtraBoldExpanded.7e9ab91e.otf","Harmond-ExtraBoldExpanded.otf"],"Harmond-ExtraBoldExpanded.otf"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"/project/home/amapic/workspace/Harmond-ExtraBoldExpanded.otf":[["Harmond-ExtraBoldExpanded.7e9ab91e.otf","Harmond-ExtraBoldExpanded.otf"],"Harmond-ExtraBoldExpanded.otf"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -197,7 +197,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38469" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34381" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
